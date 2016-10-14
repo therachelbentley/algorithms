bb2cfe9a90c04e148a81bedbc1b2d82c4e5599cd
rjohnson <rjohnson@haystax.com>
2016-10-14 08:22:24 -0400

    Expansion function

--- math.py
+++ math.py
@@ -3,7 +3,7 @@
 def _gcd(a, b):
     if(a < b):
         a, b = b, a
-    quotient, r = divmod(a,b)    
+    r = a % b    
     if r == 0:
         return b
     else:
@@ -11,4 +11,17 @@ def _gcd(a, b):
 
 def _lcm(a,b):
     return (a * b)/_gcd(a,b)
+
+# returns the base b expasion of n
+# where n has a base of 10
+def _base_expansion(n, b):
+    _remainders = []
+    q, r = divmod(n,b)
+
+    while q > 0:
+        _remainders = [r] + _remainders
+        q, r = divmod(q, b)
+        if q == 0:
+            _remainders = [r] + _remainders
+    return int("".join(str(x) for x in _remainders))
 
