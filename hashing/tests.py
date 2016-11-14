0208d4e2c0c5a397e83b3077a2dda1784b30d786
rjohnson <rjohnson@haystax.com>
2016-11-14 15:38:56 -0600

    ASCII hash function for tests

--- hashing/tests.py
+++ hashing/tests.py
@@ -6,16 +6,26 @@ phone_book = DirectAddressing(10**7)
 phone_book.add(5551234, "foo")
 phone_book.add(5552345, "bar")
 phone_book.delete(5551234)
+
 assert phone_book.find(5551234) == None
 assert phone_book.find(5552345) == "bar"
 
 
 ### HashChains Test ###
-# TODO create hash function to test
-hash_chain = HashChains(5)
-hash_chain.add(4, 'foo')
-hash_chain.add(4, 'bar')
-hash_chain.add(2, 'foo')
-hash_chain.delete(4, 'bar')
-assert hash_chain.find(4, 'bar') == -1
-assert hash_chain.find(2, 'foo') == 0
+def _hash_ascii_character(string):
+    if len(string) > 1:
+        return ord(string[0])
+
+foo_key = _hash_ascii_character("foo")
+fizz_key = _hash_ascii_character("fizz")
+bar_key = _hash_ascii_character("bar")
+
+hash_chain = HashChains(256)
+hash_chain.add(foo_key, 'foo')
+hash_chain.add(bar_key, 'bar')
+hash_chain.add(fizz_key, 'fizz')
+hash_chain.delete(bar_key, 'bar')
+
+assert hash_chain.find(bar_key, 'bar') == -1
+assert hash_chain.find(foo_key, 'foo') == 0
+assert len(hash_chain._arr[foo_key]) == 2
