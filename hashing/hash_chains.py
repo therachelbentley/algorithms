4603b720bfdcef317800533b809efe4024a66f7a
rjohnson <rjohnson@haystax.com>
2016-11-13 19:16:39 -0600

    Added HashChain

---
+++ hashing/hash_chains.py
@@ -0,0 +1,28 @@
+
+
+class HashChains():
+
+    def __init__(self, cardinality):
+        self._arr = [[] for i in range(cardinality)]
+
+    def find(self, key, query):
+        
+        try:
+            index = self._arr[key].index(query)
+        except ValueError:
+            index = -1
+
+        return index
+
+    def add(self, key, value):
+
+        index = self.find(key, value)
+        if index == -1:
+            self._arr[key].append(value)
+
+    def delete(self, key, value):
+
+        index = self.find(key, value)
+        if index != -1:
+            self._arr[key].pop(index)
+
--- hashing/tests.py
+++ hashing/tests.py
@@ -1,9 +1,21 @@
 from algorithms.hashing.direct_addressing import DirectAddressing
+from algorithms.hashing.hash_chains import HashChains
 
-
+### DirectAddressing Test ###
 phone_book = DirectAddressing(10**7)
 phone_book.add(5551234, "foo")
 phone_book.add(5552345, "bar")
 phone_book.delete(5551234)
 assert phone_book.find(5551234) == None
 assert phone_book.find(5552345) == "bar"
+
+
+### HashChains Test ###
+# TODO create hash function to test
+hash_chain = HashChains(5)
+hash_chain.add(4, 'foo')
+hash_chain.add(4, 'bar')
+hash_chain.add(2, 'foo')
+hash_chain.delete(4, 'bar')
+assert hash_chain.find(4, 'bar') == -1
+assert hash_chain.find(2, 'foo') == 0
