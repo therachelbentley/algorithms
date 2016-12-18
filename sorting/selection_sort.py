234d695294dd392bf4eaced2bd60a75bab07ccaa
rjohnson <rjohnson@haystax.com>
2016-12-18 16:23:00 -0600

    Implemented selection sort algorithm

---
+++ sorting/selection_sort.py
@@ -0,0 +1,21 @@
+
+
+class SelectionSort:
+
+    def __init__(self, arr):
+        self._arr = arr
+
+    def sort_arr(self):
+
+    	for i in range(len(self._arr)):
+
+    		min_item = i
+
+    		for j in range(i + 1, len(self._arr)):
+    			if self._arr[min_item] > self._arr[j]:
+    				min_item = j
+
+    		self._arr[i], self._arr[min_item] = self._arr[min_item], self._arr[i]
+
+    		
+    	return self._arr
