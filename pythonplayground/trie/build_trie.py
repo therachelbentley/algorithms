cd29c58c0231bba6c5ff945f6e7deb79536f20ac
rjohnson <rjohnson@haystax.com>
2017-06-10 12:50:45 -0500

    Update trie functions to handle cases where a pattern is a prefix of another pattern.

--- pythonplayground/trie/build_trie.py
+++ pythonplayground/trie/build_trie.py
@@ -7,7 +7,7 @@ def build_trie(strings):
             strings (list): A list of strings.
         Output:
             A dictionary where the keys are the node ids and the values
-            are a dictionary of the node's children.
+            are a dictionary of the node"s children.
     """
 
     # initialize trie
@@ -18,14 +18,16 @@ def build_trie(strings):
 
     for string in strings:
         current_node = 0
-        for character in string:
+        last_char = len(string) - 1
+
+        for i, character in enumerate(string):
 
             # if the current node does not exist in the dict then add it
             if not current_node in trie:
                 trie[current_node] = {}
 
-            # if the character is already a child of the current node
-            # then set the current node to be the character node's id
+            # if character is already a child of current_node
+            # then current_node now becomes that child's id
             # else create a new node for that character
             if character in trie[current_node]:
                 current_node = trie[current_node][character]
@@ -34,15 +36,26 @@ def build_trie(strings):
                 current_node = new_node
                 new_node += 1
 
+            # if this is the last character in the pattern
+            # then make one of it children indicate its
+            # a leaf node
+            if i == last_char:
+                if not current_node in trie:
+                    trie[current_node] = {}
+                trie[current_node]["LEAF"] = string
+
     return trie
 
 
 ### TESTS ###
 case_one = ["ATA"]
-assert build_trie(case_one) == {0: {"A": 1}, 1: {"T": 2}, 2: {"A": 3}}
+assert build_trie(case_one) == {0: {"A": 1}, 1: {"T": 2}, 2: {"A": 3}, 3: {"LEAF": "ATA"}}
 
 case_two = ["AT", "AG", "AC"]
-assert build_trie(case_two) == {0: {"A": 1}, 1: {"G": 3, "T": 2, "C": 4}}
+assert build_trie(case_two) == {0: {"A": 1}, 1: {"G": 3, "T": 2, "C": 4}, 2: {"LEAF": "AT"}, 3: {"LEAF": "AG"}, 4: {"LEAF": "AC"}}
 
 case_three = ["ATAGA", "ATC", "GAT"]
-assert build_trie(case_three) == {0: {"A": 1, "G": 7}, 1: {"T": 2}, 2: {"A": 3, "C": 6}, 3: {"G": 4}, 4: {"A": 5}, 7: {"A": 8}, 8: {"T": 9}}
+assert build_trie(case_three) == {0: {"G": 7, "A": 1}, 1: {"T": 2}, 2: {"C": 6, "A": 3}, 3: {"G": 4}, 4: {"A": 5}, 5: {"LEAF": "ATAGA"}, 6: {"LEAF": "ATC"}, 7: {"A": 8}, 8: {"T": 9}, 9: {"LEAF": "GAT"}}
+
+case_four = ["AT", "A", "AG"]
+assert build_trie(case_four) == {0: {"A": 1}, 1: {"G": 3, "T": 2, "LEAF": "A"}, 2: {"LEAF": "AT"}, 3: {"LEAF": "AG"}}
--- pythonplayground/trie/trie_matching.py
+++ pythonplayground/trie/trie_matching.py
@@ -2,26 +2,29 @@ from trie.build_trie import  build_trie
 
 
 def trie_matching(text, trie):
-    # keep track of the pattern it"s matching on
-    pattern = ""
+    # keep track of the pattern it's matching on
+    patterns = []
 
     # start at root node
-    v = trie[0] # root
+    v = trie[0]
+    last_char = len(text) - 1
 
-    # step through text to find matching
-    for char in text:
-        # reached the end of that pattern
-        if v is None:
-            return pattern
+    # step through text to find matches
+    for i, char in enumerate(text):
+        # reached the end of a pattern
+        if v.get("LEAF"):
+            patterns.append(v["LEAF"])
+
         # check if char is in the current trie node
-        elif char in v:
-            pattern += char
-            v = trie.get(v[char], None)
-            if v is None:
-                return pattern
+        if char in v:
+            v = trie[v[char]]
+            if i == last_char and v.get("LEAF"):
+                patterns.append(v["LEAF"])
         # char does not match any nodes
         else:
-            return None
+            return patterns
+
+    return patterns
 
 
 def match(text, patterns):
@@ -44,13 +47,13 @@ def match(text, patterns):
         return result
 
     # build trie from patterns
-    trie = build_trie(patterns)    
+    trie = build_trie(patterns)
 
     # each char in text could be the start of a pattern
     i = 0
     while text:
         match = trie_matching(text, trie)
-        if match is not None:
+        if match:
             result.append((i, match))
         text = text[1:]
         i += 1
@@ -61,7 +64,7 @@ def match(text, patterns):
 ### TESTS ###
 case_one_text = "AAA"
 case_one_patterns = ["AA"]
-assert match(case_one_text, case_one_patterns) == [(0, "AA"), (1, "AA")]
+assert match(case_one_text, case_one_patterns) == [(0, ["AA"]), (1, ["AA"])]
 
 case_two_text = "AA"
 case_two_patterns = ["T"]
@@ -70,4 +73,8 @@ assert match(case_two_text, case_two_patterns) == []
 
 case_three_text = "AATCGGGTTCAATCGGGGT"
 case_three_patterns = ["ATCG", "GGGT"]
-assert match(case_three_text, case_three_patterns) == [(1, "ATCG"), (4, "GGGT"), (11, "ATCG"), (15, "GGGT")]
+assert match(case_three_text, case_three_patterns) == [(1, ["ATCG"]), (4, ["GGGT"]), (11, ["ATCG"]), (15, ["GGGT"])]
+
+case_four_text = "AATCG"
+case_four_patterns = ["ATCG", "GGGT", "AT"]
+assert match(case_four_text, case_four_patterns) == [(1, ['AT', 'ATCG'])]
